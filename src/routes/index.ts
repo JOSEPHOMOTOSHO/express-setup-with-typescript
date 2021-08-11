58597033b197453110a0d8ffa56869117f7e5313
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-08-11 23:09:15 +0100

    progress

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (_req, res, next) {
-  res.render('index', { title: 'Omotosho Joseph' })
+  res.render('index', { title: 'Omotosho Joseph' });
 });
 
 export default router;
